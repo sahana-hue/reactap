0754cf15977e9a097f8046b9540577b3b1b2b8d4
sahana-hue <sahanabagalakoti@gmail.com>
2026-04-18 12:21:11 +0530

    done

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,31 +1,44 @@
 import React, { useState } from 'react'
-
+import "./App.css"
+import Todolist from './components/todolist';
 const App = () => {
   const [list, setList] = useState([])
   const [valued, setValued] = useState("")
   const handleAdd = () => {
+    if(valued.trim() === "") return;
     setList([...list, valued])
     setValued("")
   }
+
+  const handleDelete = (indexToDelete) => {
+    setList(list.filter((_, index) => index !== indexToDelete));
+  }
+
   return (
-    <div>
-      <h1>Todo List</h1>
-      <input type="text" value={valued} onChange={
-        (e) => {
-          setValued(e.target.value);
-        }
-      } />
-      <button onClick={handleAdd} onKeyDown={(e)=>{
-        if(e.key==="Enter"){
-          handleAdd()
-        }
-      }}
-        >Add</button>
-      <ul>
-        {list.map((i) => (
-          <li key={Math.random()}>{i}</li>
-        ))}
-      </ul>
+    <div className="todo-app">
+      <div className="todo-container">
+        <h1 className="gh">Todo List</h1>
+        <div className="input-box">
+          <input type="text" value={valued} onChange={
+            (e) => {
+              setValued(e.target.value);
+            }
+          } />
+          <button 
+            onClick={handleAdd} 
+            onKeyDown={(e)=>{
+              if(e.key==="Enter"){
+                handleAdd()
+              }
+            }}
+          >
+            Add
+          </button>
+        </div>
+        <div className="todo-list-wrapper">
+          <Todolist list={list} deletetodo={handleDelete} />
+        </div>
+      </div>
     </div>
   )
 }
